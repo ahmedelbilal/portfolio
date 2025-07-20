5ec5a2cd14fd619a16060298330dfd017214780e
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-07-20 22:21:26 +0300

    fix: return type

--- src/utils/get-icon.ts
+++ src/utils/get-icon.ts
@@ -2,6 +2,8 @@ import * as Icons from "lucide-react";
 
 type IconName = keyof typeof Icons;
 
-export default function getIcon(name: string | null): React.ElementType {
-  return (Icons[name as IconName] as React.ElementType) || Icons.Globe;
+export default function getIcon(
+  name: string | null
+): React.ElementType<Icons.LucideProps> {
+  return (Icons[name as IconName] as Icons.LucideIcon) || Icons.Globe;
 }
